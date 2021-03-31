e5fef30cfb96e766d28142d0ff6cafb9a4a12ee5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-30 21:10:27 -0300

    chore: add cetralized error handler

--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -2,52 +2,43 @@ const express = require('express');
 const router = express.Router();
 const postsService = require('../service/postsService');
 
-router.get('/posts', async(req, res) => {
-    const posts = await postsService.getPosts();
-    // posts = { test: 2 };
-    res.json(posts);
+router.get('/posts', async(req, res, next) => {
+    try {
+        const posts = await postsService.getPosts();
+        res.json(posts);
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.post('/posts', async(req, res) => {
+router.post('/posts', async(req, res, next) => {
     const post = req.body;
     try {
         const newPost = await postsService.savePost(post);
         res.status(201).json(newPost);
     } catch (e) {
-        if (e.message === 'Post already exists') {
-            res.status(409).send(e.message);
-        } else {
-            res.status(500).send(e.message);
-        }
+        next(e);
     }
 });
 
 
-router.put('/posts/:id', async(req, res) => {
+router.put('/posts/:id', async(req, res, next) => {
     const post = req.body;
     try {
         await postsService.updatePost(req.params.id, post);
         res.status(204).end();
     } catch (e) {
-        if (e.message === 'Post already exists') {
-            res.status(404).send(e.message);
-        } else {
-            res.status(500).send(e.message);
-        }
+        next(e);
     }
 });
 
 
-router.delete('/posts/:id', async(req, res) => {
+router.delete('/posts/:id', async(req, res, next) => {
     try {
         await postsService.deletePost(req.params.id);
         res.status(204).end();
     } catch (e) {
-        if (e.message === 'Post already exists') {
-            res.status(404).send(e.message);
-        } else {
-            res.status(500).send(e.message);
-        }
+        next(e);
     }
 });
 
